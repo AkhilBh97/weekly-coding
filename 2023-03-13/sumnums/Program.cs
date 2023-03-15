35e82aa2ded3c892bd30603070a0a9db153d7f6b
AkhilBh97 <akhil416@revature.net>
2023-03-15 09:19:17 -0700

    Coding challenge for the week of 3/12/2023

---
+++ 2023-03-13/sumnums/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main(string[] args){
+        int a = Int32.Parse(args[0]);
+        int b = Int32.Parse(args[1]);
+        Console.WriteLine($"{a} + {b} = {Sum(a,b)}");
+    }
+
+    public static int Sum(int a, int b){
+        return a + b;
+    }
+}
