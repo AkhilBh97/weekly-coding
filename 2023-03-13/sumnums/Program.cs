c65e4417ce59d1e4a1c9292bef3a14f51f631bb5
AkhilBh97 <akhil416@revature.net>
2023-03-15 09:22:48 -0700

    .

--- 2023-03-13/sumnums/Program.cs
+++ 2023-03-13/sumnums/Program.cs
@@ -3,6 +3,7 @@
     public static void Main(string[] args){
         int a = Int32.Parse(args[0]);
         int b = Int32.Parse(args[1]);
+        //dotnet run <a> <b>
         Console.WriteLine($"{a} + {b} = {Sum(a,b)}");
     }
 
