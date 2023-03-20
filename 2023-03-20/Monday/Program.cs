ead418f65f3f4117a4bb8cc150111d1388f0a75c
AkhilBh97 <akhil416@revature.net>
2023-03-20 12:24:28 -0700

    Challenge for March 20, 2023

---
+++ 2023-03-20/Monday/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        //CheckEquality(1, true) -> false
+        var ceIB = CheckEquality(1, true);
+        Console.WriteLine($"(1, true) => {ceIB}");
+        //CheckEquality(0, "0") -> false
+        var ceIS = CheckEquality(0, "0");
+        Console.WriteLine($"(0, \"0\") => {ceIS}");
+        //CheckEquality(1,  1) -> true
+        var ceII = CheckEquality(1, 1);
+        Console.WriteLine($"(1, 1) => {ceII}");
+    }
+
+    public static bool CheckEquality<T, U>(T a, U b){
+        //check type equality
+        if (a.GetType() != b.GetType()) return false;
+        return a.Equals(b);
+    }
+}
