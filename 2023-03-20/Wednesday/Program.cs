19d696c4906d7158aae521abc5ceb4bd5c934d1b
AkhilBh97 <akhil416@revature.net>
2023-03-23 07:22:23 -0700

    Code for March 23, 2023

---
+++ 2023-03-20/Wednesday/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main(string[] args){
+        int v = Int32.Parse(args[0]);
+        int c = Int32.Parse(args[1]);
+        Console.WriteLine($"Voltage: {v}\nCurrent: {c}\nPower: {CircuitPower(v,c)}");
+    }
+
+    public static int CircuitPower(int v, int c){
+        return v * c;
+    }
+}
